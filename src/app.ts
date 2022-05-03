1f542993a7692be380e19f230e9b40df45fe5320
Alexis Aoun <alexis.aoun@hotmail.com>
2022-05-03 15:15:04 +0200

    feat: graphql hello world

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,29 @@
 import "dotenv/config";
 import express from "express";
 import logger from "./log/logger";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World !");
-});
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {
+  hello: () => {
+    return 'Hello World';
+  },
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
 
 app.listen(port, () => {
   logger.log("info", "Server listening to port " + port);
