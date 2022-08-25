7f65816d88bf01863d1fd690bb1199fddc090db5
Alexis Aoun <alexis.aoun@hotmail.com>
2022-08-26 00:35:01 +0200

    WIP models and db

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,48 @@
 #[macro_use]
 extern crate rocket;
 
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    let python_tag = Tag {
+        name: String::from("Python"),
+        tag_type: TagType::Tech,
+        color: String::from("blue"),
+    };
+    let my_first_article = Article {
+        title: String::from("A python project"),
+        content: String::from("this a super nice project using python"),
+        tags: python_tag,
+    };
+    let output = String::from("My portfolio \n") 
+    + &my_first_article.title + &String::from("\n")
+    + &my_first_article.content + &String::from("\n")
+    + &my_first_article.tags.name + &String::from("\n")
+    + &my_first_article.tags.color + &String::from("\n"); 
+
+    return output;
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+//TODO refactor
+enum TagType{
+    Tech,
+    Experience,
+    Domaine,
+}
+
+struct Tag {
+    name: String,
+    tag_type: TagType,
+    color: String, //in hex format
+}
+
+struct Article { 
+    title: String,
+    content: String,
+    tags: Tag, //TODO should be an array
 }
