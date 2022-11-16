691b503095c981e2b37c3fc6da6c815fdad4922c
Alexis Aoun <alexis.aoun@hotmail.com>
2022-11-17 00:26:16 +0100

    fix: tag all route

--- src/routes/tag.rs
+++ src/routes/tag.rs
@@ -2,7 +2,7 @@ use crate::handlers::tag;
 use crate::models::tag::Tag;
 use rocket::serde::json::Json;
 
-#[get("/article/all")]
+#[get("/tag/all")]
 pub async fn get_all_tags() -> Json<Vec<Tag>> {
     Json(tag::get_all_tags().await)
 }
